183808dfab4bd2004d28bb2fb811ed03f02c1c7d
SHADIL VV <shadilshad508@gmail.com>
2025-08-21 19:48:27 +0530

    practiced one

---
+++ practice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Practice{
+    public static void main(String args[]){
+        int[] extra = {0,1,2,3,4};
+        System.out.println("this is how it works FYI;)");
+        for(int i=0;i<extra.length;i++){
+            System.out.println(extra[i]);
+        }
+        
+    }
+}
