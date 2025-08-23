e64f4fbccc88914a03c0471bbf536f4214d7f269
SHADIL VV <shadilshad508@gmail.com>
2025-08-23 17:05:39 +0530

    practicing java

---
+++ js.js
@@ -0,0 +1,10 @@
+console.log('Hello')
+console.log(`i like pizza`)
+
+window.alert("Guys, Welcome to this shitty website");
+window.alert("Haha just kidding brotha");
+window.alert("but still aint that bad");
+window.alert("so welcome home!");
+
+document.getElementById("Hid").textContent="Welcome JS JS";
+document.getElementById("Hp").textContent="Wassup buddy";
