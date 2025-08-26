31953c9eded2e0d959b48019030c5714b80a7b37
SHADIL VV <shadilshad508@gmail.com>
2025-08-26 07:40:12 +0530

    weekend practice

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
 "use strict";
-alert("Welcome to our page  😄");
 
+function sayHello() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let dob = document.getElementById("date").value;
+
+  alert("Hello " + username + " 👋\n" +
+        "Password: " + password + "\n" +
+        "D.O.B: " + dob);
+}
